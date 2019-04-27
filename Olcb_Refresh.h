c8cf280629fcc5b4a54bd0c2b40e017aaae10fde
David Harris <drdpharris@gmail.com>
2019-04-26 19:10:10 -0700

    Add Arduino.h

--- Olcb_Refresh.h
+++ Olcb_Refresh.h
@@ -1,6 +1,11 @@
 #ifndef OLCB_REFRESH_H
 #define OLCB_REFRESH_H
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
 #include "processor.h"
 #include "OpenLCBHeader.h"
